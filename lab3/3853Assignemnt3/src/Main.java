dc2c9bcc8ad33a7ed7027b4cbd7fdcf873f4cdfa
krismndz <krismndz7@gmail.com>
2015-11-12 19:34:06 -0600

    Added test files to src file

--- lab3/3853Assignemnt3/src/Main.java
+++ lab3/3853Assignemnt3/src/Main.java
@@ -4,16 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		/**
 		int arg1 = Integer.parseInt(args[0]);
 		int arg2 = Integer.parseInt(args[1]);
 		int arg3 = Integer.parseInt(args[2]);
 		String arg4 = args[3];
 		String arg5 = args[4];
 		String arg6=args[5];
-		CacheSim cache = new CacheSim(arg1, arg2,arg3,arg4, arg5, arg6);**/
-		
-		test();
+		CacheSim cache = new CacheSim(arg1, arg2,arg3,arg4, arg5, arg6);	
+//		test();
 	}
 	
 	public static void test(){
